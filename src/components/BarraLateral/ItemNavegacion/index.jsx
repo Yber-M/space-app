a2c02c2d95101a0f62164ae1812adca53a89ca43
Yber-M <ybermarlon@gmail.com>
2025-01-21 11:13:19 -0500

    Se termino con la barra lateral y se le agrego dinamica a los colores e iconos segun este este activo

--- src/components/BarraLateral/ItemNavegacion/index.jsx
+++ src/components/BarraLateral/ItemNavegacion/index.jsx
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
 const ItemListaEstelizado = styled.li`
-
+    font-size: 24px;
+    margin-bottom: 30px;
+    line-height: 28px;
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    color: ${props => props.$activo ? "#7B78E5" : "#d9d9d9"};
+    font-family: ${props => props.$activo ? "GandhiSansBold" : "GandhiSansRegular"};
 `;
 
 const ItemNavegacion = ({ children, iconoActivo, iconoInactivo, activo = false }) => {
-    return <ItemListaEstelizado>
+    return <ItemListaEstelizado $activo={activo}>
         <img src={activo ? iconoActivo : iconoInactivo} alt="icono" />
         {children}
     </ItemListaEstelizado>
